89e1528d9b323e8a0489de41db80ad1b6b20d9bf
oleskarr <olesastankevic7@gmail.com>
2024-09-26 20:55:22 +0300

    update

---
+++ client/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+<ul>
+    <li><NuxtLink to="/">Home</NuxtLink></li>
+    <li><NuxtLink to="/about">About</NuxtLink></li>
+    <li><NuxtLink to="/contact">Сontact</NuxtLink></li>
+</ul>
+</template>
+<style scoped>
+ul{ 
+    list-style: none;
+    display: flex;
+    
+    color: black;
+    gap: 20px;
+}
+</style>
---
+++ client/pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+   <header>
+    <h2>Home</h2>
+   </header>
+   <main>
+    <article v-for="post in posts" :key="post.id">
+        {{ post.title }}
+        <img src="" alt="">
+        <ul>
+        <li v-for="tag in post.tags" :key="tag.id">
+                {{ tag }}
+        </li>
+        </ul>
+        <NuxtLink to="/post/${index}">interesting</NuxtLink>
+    </article>
+   </main>
+</template>
+<style scooped>
+    main{
+        gap: 10px;
+        display: flex;
+        
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+    article{
+       
+        height: max-content;
+        padding: 10px;
+       
+        border-radius: 10px;
+        background-color: #121910;
+        color: bisque;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+    }
+    ul{
+       width: max-content;
+       color:white;
+      
+      align-self: self-start;
+        display: flex;
+        flex-direction: column;
+      
+    }
+  
+</style>
+<script setup>
+    const dataTwice = await $fetch('https://dummyjson.com/posts')
+    const posts = dataTwice.posts
+    console.log(posts);
+    
+</script>
