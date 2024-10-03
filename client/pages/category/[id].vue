cedb75b8d9333c8e97f5bd7c6c9a0d104b1905be
oleskarr <olesastankevic7@gmail.com>
2024-10-03 20:47:39 +0300

    updaaate

--- client/pages/category/[id].vue
+++ client/pages/category/[id].vue
@@ -1,6 +1,23 @@
 <template>
+    <h2>Блог</h2>
 
+    <main>
+    <div class="posts">
+        <article v-for="(post, index) in posts" :key="post.id">
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.desc }}</p>
+        <NuxtLink :to="'/post/' + index">Подробнее</NuxtLink>
+      </article>
+    </div>
+    </main>
 </template>
+
 <script setup>
- 
+const { id } = useRoute().params
+
+const api = await $fetch(`http://localhost:1337/api/categories?populate=*`)
+const filteredPosts = api.data.filter(post => post.id == id)
+const posts = filteredPosts[0].posts
+
+const base_url = 'http://localhost:1337'
 </script>
