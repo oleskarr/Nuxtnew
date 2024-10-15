c750182aa53f84ff1a52aa4ea2bb36a95b00bc0d
oleskarr <olesastankevic7@gmail.com>
2024-10-15 18:45:38 +0300

    server's here

---
+++ server/src/api/my-blog/controllers/my-blog.ts
@@ -0,0 +1,7 @@
+/**
+ * my-blog controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::my-blog.my-blog');
---
+++ server/src/api/my-blog/routes/my-blog.ts
@@ -0,0 +1,7 @@
+/**
+ * my-blog router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::my-blog.my-blog');
