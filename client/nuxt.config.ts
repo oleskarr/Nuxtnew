73693f79acaa3279d3cef6e6ceb2ac4d9752148c
oleskarr <olesastankevic7@gmail.com>
2024-11-14 19:48:30 +0300

    апдэтос

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devServer:{
+    host: '0.0.0.0',
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
