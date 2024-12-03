114633f5886088e663b1233c92402b0fcaa6be72
oleskarr <olesastankevic7@gmail.com>
2024-12-03 19:43:14 +0300

    *

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -44,25 +44,7 @@
       </div>
     </div>
 
-    <!-- Кнопка для изменения ширины панели -->
-    <button
-      @click="togglePanelWidth"
-      class="absolute top-1/2 transform -translate-y-1/2 bg-gray-300 hover:bg-gray-300 cursor-pointer rounded-full w-12 h-12 flex items-center justify-center transition-all duration-300"
-      :style="{ left: `calc(${panelWidth}% - 24px)` }"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 transition-transform duration-300"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path 
-          :d="panelWidth === 80 ? 'M9 5l7 7-7 7' : 'M15 19l-7-7 7-7'" 
-          stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-      </svg>
-    </button>
-
+    
     
   </div>
 </template>
