8c3977297ba32d1d0be933e4a96dfcea8be2ab88
oleskarr <olesastankevic7@gmail.com>
2024-12-03 19:29:57 +0300

    8**

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,38 +1,5 @@
 <template>
-  <div class="flex h-screen relative">
- 
-
-    <!-- Кнопка для изменения ширины панели -->
-    <button
-      @click="togglePanelWidth"
-      class="absolute top-1/2 transform -translate-y-1/2 bg-gray-300 hover:bg-gray-300 cursor-pointer rounded-full w-12 h-12 flex items-center justify-center transition-all duration-300"
-      :style="{ left: `calc(${panelWidth}% - 24px)` }"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 transition-transform duration-300"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path 
-          :d="panelWidth === 80 ? 'M9 5l7 7-7 7' : 'M15 19l-7-7 7-7'" 
-          stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-      </svg>
-    </button>
-
-    <!-- Правая панель -->
-    <div class="flex-1 p-6 text-center">
-      <div class="absolute top-0 w-full">
-        <Head/>
-      </div>
-      <h2 class="text-3xl lg:text-5xl font-bold text-gray-800 mb-6 lg:mb-14 tracking-wide">
-        Добро пожаловать в наш блог
-      </h2>
-      <h5 class="text-lg">Search for your subject</h5>
-      <Navbar />
-    </div>
-  </div>
+  grgr
 </template>
 
 <script setup>
