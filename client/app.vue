592c21a9fcfeabb835ac49fe67b145a389a8901e
oleskarr <olesastankevic7@gmail.com>
2024-12-03 19:16:23 +0300

    hgjg

--- client/app.vue
+++ client/app.vue
@@ -1,9 +1,10 @@
 <template>
+  <h1>rtyjhhu</h1>
  
-  <NuxtPage />
+  <!-- <NuxtPage /> -->
   
 </template>
-<script setup>
+<!-- <script setup>
   const base_url = 'https://d19d642231aa.vps.myjino.ru' 
   const api = await $fetch(`https://d19d642231aa.vps.myjino.ru
 /api/my-blog?populate=*`)
@@ -22,7 +23,7 @@
       class:'w-full h-screen fixed inset-0 p-0'
     }
   })
-</script>
+</script> -->
 <style>
 
 
